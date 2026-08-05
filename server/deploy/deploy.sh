9cf075a7a9d09a823b9a725a1ead721eb79f7aba
tiss-develop <tiss6278git>
2026-08-05 14:16:56 +0700

    test

--- server/deploy/deploy.sh
+++ server/deploy/deploy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")/.." || exit
 
+TEST_FLAG=""
+if [ "$1" == "--test" ]; then
+    TEST_FLAG="--test"
+fi
+
 git pull
 
 docker stop ws-meex-app 2>/dev/null
@@ -8,6 +13,6 @@ docker rm ws-meex-app 2>/dev/null
 
 docker build -t meex-server -f deploy/Dockerfile .
 
-docker run -d --restart unless-stopped --name ws-meex-app -p 8765:8765 meex-server
+docker run -d --restart unless-stopped --name ws-meex-app -p 8765:8765 meex-server python python/server.py $TEST_FLAG
 
 docker logs ws-meex-app
